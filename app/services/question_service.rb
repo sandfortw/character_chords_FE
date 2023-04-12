01c7955ace30bf816130025a1877a888f815e570
Weston Sandfort <sandfortw@gmail.com>
2023-04-12 13:05:21 -0600

    remove duplicated variable from quiz and question services

--- app/services/question_service.rb
+++ app/services/question_service.rb
@@ -1,5 +1,5 @@
-class QuestionService < Application_Service
-
+class QuestionService < ApplicationService
+  
 
   def questions(quiz_id)
     get_url("#{@@base_url}/chordsapi/v1/themes/#{quiz_id}/questions")
--- app/services/quiz_service.rb
+++ app/services/quiz_service.rb
@@ -1,6 +1,5 @@
 class QuizService < ApplicationService
 
-
   def quizzes
     get_url("#{@@base_url}/chordsapi/v1/themes")
   end
