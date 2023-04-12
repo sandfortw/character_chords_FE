457cf13d300f5159cace3f6980a64f0271c00125
James Taylor <jrtaylor0614@gmail.com>
2023-04-12 17:20:08 -0600

    Test: login, logout, and home links

--- spec/home/index_spec.rb
+++ spec/home/index_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe 'Home Page', type: :feature do
 
   describe 'Link to Log in' do
     context "when successful" do
-
-      it "When clicked, we are taken to the login page with a welcome message (happy path)" do
+      it "When clicked, we are taken to the login page with a welcome message" do
+        expect(page).to have_link("Sign in with Spotify")
         login_with_oauth
         expect(current_path).to eq(root_path)
         expect(page).to have_content("Welcome, Test User")
@@ -17,11 +17,32 @@ RSpec.describe 'Home Page', type: :feature do
     end 
     
     context "when NOT successful" do
-      it 'when clicked, we are taken to the login page (sad path)' do
+      it 'when clicked, we are taken to the login page' do
         click_on('Sign in with Spotify')
         expect(current_path).to eq(root_path)
         expect(page).to have_content("Spotify Login Failed")
       end
     end 
   end
+
+  describe "Link to logout" do
+    it "when clicked, the user is logged out with a message notification them " do
+      login_with_oauth
+      expect(page).to have_link("Logout")
+      click_link "Logout"
+      expect(page).to_not have_link("Logout")
+      expect(page).to have_link("Sign in with Spotify")
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("You have been logged out")
+    end
+  end
+
+  describe "Link to go home " do
+    it "when clicked, the user is redirected to the home page" do
+      expect(page).to have_link("Home")
+      click_link("Home")
+      expect(current_path).to eq(root_path)
+    end
+  end
+  
 end
