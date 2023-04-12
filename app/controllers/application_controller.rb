b6aa619e420e600474f87fa6429e848133931d6d
Weston Sandfort <sandfortw@gmail.com>
2023-04-11 18:36:00 -0600

    add code to pass first test for home page and login is working as designed on localhost

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  before_action :current_user
+
+  def current_user
+    @_current_user ||= RSpotify::User.new(session[:user_hash]) if session[:user_hash]
+  end
+
+  def current_user?
+    current_user ? true : false
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  get '/auth/spotify/callback', to: 'users#spotify'
-
+  get '/auth/spotify/callback', to: 'users#create'
+  get 'auth/failure', to: 'users#failure'
 end
