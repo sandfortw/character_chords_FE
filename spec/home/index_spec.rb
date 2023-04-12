dac3b8bcbc818ca84a42f1f0df475bbd8f176cbc
James Taylor <jrtaylor0614@gmail.com>
2023-04-12 16:28:27 -0600

    Test: home page login and logout

--- spec/home/index_spec.rb
+++ spec/home/index_spec.rb
@@ -1,16 +1,27 @@
 require 'rails_helper'
 
-
 RSpec.describe 'Home Page', type: :feature do
 
   before do
     visit root_path
   end
+
   describe 'Link to Log in' do
-    it 'when clicked, we are taken to the login page (sad path)' do
-      click_on('Sign in with Spotify')
-      expect(current_path).to eq(root_path)
-      expect(page).to have_content("Spotify Login Failed")
-    end
+    context "when successful" do
+
+      it "When clicked, we are taken to the login page with a welcome message (happy path)" do
+        login_with_oauth
+        expect(current_path).to eq(root_path)
+        expect(page).to have_content("Welcome, Test User")
+      end
+    end 
+    
+    context "when NOT successful" do
+      it 'when clicked, we are taken to the login page (sad path)' do
+        click_on('Sign in with Spotify')
+        expect(current_path).to eq(root_path)
+        expect(page).to have_content("Spotify Login Failed")
+      end
+    end 
   end
 end
