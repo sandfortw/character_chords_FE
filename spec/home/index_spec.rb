ea65505f5801066144d226c67ee5e6f2badee0f7
Weston Sandfort <sandfortw@gmail.com>
2023-04-11 18:34:48 -0600

    create first test for login

--- spec/home/index_spec.rb
+++ spec/home/index_spec.rb
@@ -7,8 +7,10 @@ RSpec.describe 'Home Page', type: :feature do
     visit root_path
   end
   describe 'Link to Log in' do
-    it 'when clicked, we are taken to the login page' do
+    it 'when clicked, we are taken to the login page (sad path)' do
       click_on('Sign in with Spotify')
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("Spotify Login Failed")
     end
   end
 end
