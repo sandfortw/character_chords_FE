554691ea17ec46b881c5c9049980d9dad5d3af82
Weston Sandfort <sandfortw@gmail.com>
2023-04-13 15:34:24 -0600

    remove inaccurate comments

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,12 +4,12 @@ class CharactersController < ApplicationController
   def create
     answer_params = params.permit(answers: {})
     quiz_id = params[:quiz_id]
-    character = CharacterFacade.new(answer_params, quiz_id).character #returns a character object
+    character = CharacterFacade.new(answer_params, quiz_id).character
     session[:character] = character
     redirect_to characters_path
   end
 
   def index
-    @character = Character.new(current_character.deep_symbolize_keys) #for some reason it is transformed into a hash here. Idk if character objects can be stored in sessions.
+    @character = Character.new(current_character.deep_symbolize_keys)
   end
 end
