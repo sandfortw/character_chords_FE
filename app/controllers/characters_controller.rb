a2ee82ff624c8f08b39f8a2bc44ffc60bd8f2ea4
Weston Sandfort <sandfortw@gmail.com>
2023-04-13 15:11:53 -0600

    character object is moved into character index as a hash

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,11 +4,12 @@ class CharactersController < ApplicationController
   def create
     answer_params = params.permit(answers: {})
     quiz_id = params[:quiz_id]
-    session[:character] = CharacterFacade.new(answer_params, quiz_id).character
+    character = CharacterFacade.new(answer_params, quiz_id).character #returns a character object
+    session[:character] = character
     redirect_to characters_path
   end
 
   def index
-    @character = current_character
+    @character = current_character #for some reason it is transformed into a hash here. Idk if character objects can be stored in sessions.
   end
 end
