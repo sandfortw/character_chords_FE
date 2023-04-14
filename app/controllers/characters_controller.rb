1ec644ea51b9d3a61fb16a4ec52d9b58f0cee8d1
Weston Sandfort <sandfortw@gmail.com>
2023-04-13 20:21:05 -0600

    feature: quizzes cannot be submitted with missing fields

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,9 +4,14 @@ class CharactersController < ApplicationController
   def create
     answer_params = params.permit(answers: {})
     quiz_id = params[:quiz_id]
-    character = CharacterFacade.new(answer_params, quiz_id).character
-    session[:character] = character
-    redirect_to characters_path
+    if answer_params[:answers].to_h.size == params[:question_count]
+      character = CharacterFacade.new(answer_params, quiz_id).character
+      session[:character] = character
+      redirect_to characters_path
+    else
+      flash[:error] = "All fields must be selected!"
+      redirect_to quiz_path(quiz_id)
+    end
   end
 
   def index
