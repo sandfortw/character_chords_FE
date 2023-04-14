69b681e06d21d62171fd461868eccdbffac6eab0
James Taylor <jrtaylor0614@gmail.com>
2023-04-14 09:31:56 -0600

    Test: quiz #show, quiz title

--- spec/features/quizzes/show_spec.rb
+++ spec/features/quizzes/show_spec.rb
@@ -1,4 +1,18 @@
+require "rails_helper"
+
 RSpec.describe 'Quiz Show Page', type: :feature do
+  
+  describe "When visiting the quiz show page" do
+
+    before :each do
+      visit quiz_path(2)
+    end
+    
+    it " The title of the quiz is show " do
+      expect(page).to have_content("ALIGNMENT QUIZ")
+    end
+    
+  end
 
   
 end
