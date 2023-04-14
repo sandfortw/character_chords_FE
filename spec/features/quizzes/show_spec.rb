0a44c09ec49a2ce78285ca539e6a14fa528b7b3f
James Taylor <jrtaylor0614@gmail.com>
2023-04-14 12:52:34 -0600

    test: Quiz #show answer questions

--- spec/features/quizzes/show_spec.rb
+++ spec/features/quizzes/show_spec.rb
@@ -11,8 +11,27 @@ RSpec.describe 'Quiz Show Page', type: :feature do
     it " The title of the quiz is show " do
       expect(page).to have_content("ALIGNMENT QUIZ")
     end
-    
-  end
 
-  
+    context "When each question is answered and you click submit " do
+      it "redirects you to the character index page where you see your results" do
+       
+        within('form') do
+          choose("answers['1good_evil']", option: "0")
+          choose("answers['2good_evil']", option: "0")
+          choose("answers['3good_evil']", option: "0")
+          choose("answers['4good_evil']", option: "0")
+          choose("answers['5good_evil']", option: "0")
+          choose("answers['6good_evil']", option: "0")
+          choose("answers['7chaotic_lawful']", option: "0")
+          choose("answers['8chaotic_lawful']", option: "0")
+          choose("answers['9chaotic_lawful']", option: "0")
+          choose("answers['10chaotic_lawful']", option: "0")
+          choose("answers['11chaotic_lawful']", option: "0")
+          choose("answers['12chaotic_lawful']", option: "0")
+        end
+        
+        click_button "Get your results"
+      end
+    end
+  end
 end
