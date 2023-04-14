c83401567b75d0553c173465e75764c40de80cfc
Weston Sandfort <sandfortw@gmail.com>
2023-04-14 13:22:32 -0600

    remove a pry

--- app/facades/character_facade.rb
+++ app/facades/character_facade.rb
@@ -7,7 +7,6 @@ class CharacterFacade
 
   def character
     answer_hash =  @params[:answers].to_h
-    require 'pry'; binding.pry
     CharacterService.new.get_character({good_score: good_score(answer_hash), lawful_score: lawful_score(answer_hash), quiz_id: @quiz_id})
   end
 
