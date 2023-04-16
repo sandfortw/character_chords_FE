7ea6d3f65fca5b37674c8e506453d5f2c6f3dc34
Weston Sandfort <sandfortw@gmail.com>
2023-04-16 16:36:04 -0600

    fix playlist show tests

--- spec/features/characters/index_spec.rb
+++ spec/features/characters/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Character Index', type: :feature do
     before(:each) do 
       character_data = 
       {"links"=>{"image"=>"https://upload.wikimedia.org/wikipedia/commons/b/ba/Michael_Cohen_in_2019.png"},
-      "data"=>{"type"=>"character", "theme_id"=>1, "attributes"=>{"name"=>"Michael Cohen", "quiz"=>"lawyer", "alignment"=>"Neutral Evil"}}}
+      "data"=>{"id" => "1", "type"=>"character", "theme_id"=>1, "attributes"=>{"name"=>"Michael Cohen", "quiz"=>"lawyer", "alignment"=>"Neutral Evil", "character_id" => "1"}}}
 
       allow_any_instance_of(ApplicationController).to receive(:current_character).and_return(Character.new(character_data.deep_symbolize_keys))
       visit "/characters"
@@ -16,7 +16,6 @@ RSpec.describe 'Character Index', type: :feature do
       expect(page).to have_content("Michael Cohen")
       expect(page).to have_content("Neutral Evil")
       expect(page).to have_selector("img[src='https://upload.wikimedia.org/wikipedia/commons/b/ba/Michael_Cohen_in_2019.png']")
-
     end
 
     it "the page will have a form asking you what type of music you want to have a playlist returned for" do 
@@ -25,6 +24,18 @@ RSpec.describe 'Character Index', type: :feature do
       expect(page).to have_field(:query)
     end
 
+    it "filling out the form on characters page will take me to the create action which creates a playlist", :vcr do
+      visit "/characters"
+      fill_in :query, with: "90s rap"
+      click_button "Create Playlist"
+      expect(current_path).to match(/\/playlists\/\d+/)      
+      expect(page).to have_content("10 Recommended Songs")
+      expect(page).to have_css('div#song', count: 10)
+      page.all('div.song').each do |song_div|
+        expect(song_div).not_to be_empty
+      end     
+    end
+    
     context "when logged in" do
       before do
         login_with_oauth
