6c60d2bab439c365bc5a02c78f40c78e20e21d19
Weston Sandfort <sandfortw@gmail.com>
2023-04-19 17:28:42 -0600

    add email toggle

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :characters, only: %i[create index]
   resources :playlists, only: %i[create show]
 
-  get '/playlists/:id/open_with_spotify', to: 'playlists#open_with_spotify', as: 'open_with_spotify_playlist'
+  post '/playlists/:id/open_with_spotify', to: 'playlists#open_with_spotify', as: 'open_with_spotify_playlist'
   get '/auth/spotify/callback', to: 'users#create'
   get 'auth/failure', to: 'users#failure'
   get '/logout', to: 'users#logout'
